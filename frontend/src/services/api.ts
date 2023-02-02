e273d846d7d29456e2077aeaa7c10639f4d7e722
alangalvao <alan.galvao.galvao@gmail.com>
2023-02-02 20:59:00 -0300

    atualização de backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined) {
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL:'http://localhost:3333',
+        baseURL:'https://universo-burguer-backend.vercel.app',
         headers:{
             Authorization: `Bearer ${cookies[`@universoBurguer.token`]}`
         }
